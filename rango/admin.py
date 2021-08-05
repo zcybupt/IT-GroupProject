4dd0129cf60287a59fe0a72f8ca3724a72a3ada4
Hangchang Zhang <zhanghcdream@outlook.com>
2021-08-05 20:55:05 +0800

    change admin page

--- rango/admin.py
+++ rango/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
-from rango.models import Category, Page, UserProfile
+from rango.models import Category, Page, UserProfile, Movie, Review
 
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genres', 'imdb_id' , 'release_year', 'rating')
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page, PageAdmin)
+class Reviewadmin(admin.ModelAdmin):
+    list_display = ('movie', 'user', 'title', 'content', 'time', 'likes', 'rating')
+admin.site.register(Movie, MovieAdmin)
+#admin.site.register(Category, CategoryAdmin)
+#admin.site.register(Page, PageAdmin)
 admin.site.register(UserProfile)
+admin.site.register(Review, Reviewadmin)
